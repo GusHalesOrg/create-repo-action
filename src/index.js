c93a999dfe74af3e15672d136e26a790eaceb507
agustinhales <agustin.hales@fanduel.com>
2023-04-07 16:45:04 +0100

    reverting back octokit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import { Octokit } from "octokit";
+
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -8,32 +9,28 @@ const targetRepoDescription = core.getInput('repo-description');
 const targetOrgName = github.context.payload.repository.owner.login;
 const templateName = core.getInput('template-name');
 
-var createRepoData = JSON.stringify(
-    {
-        "name": targetRepoName,
-        "private": true,
-        "owner": targetOrgName,
-        "description": targetRepoDescription,
-        "include_all_branches": false
-    }
-);
-
-var config = {
-    method: 'post',
-    url: 'https://api.github.com/repos/' + targetOrgName + '/' + templateName + '/generate',
-    headers: {
-        'Accept': 'application/vnd.github.v3+json',
-        'Authorization': 'token ' + ghToken,
-        'Content-Type': 'application/json'
-    },
-    data: createRepoData
-};
-axios(config)
-    .then(function (response) {
-        console.log("Repo " + targetRepoName + ' created successfully!');
-        core.setOutput("repo-url", "https://github.com/" + targetOrgName + "/" + targetRepoName);
+const octokit = new Octokit({
+    auth: ghToken
+})
+async function main() {
+    await octokit.request('POST /repos/{template_owner}/{template_repo}/generate', {
+        template_owner: targetOrgName,
+        template_repo: templateName,
+        owner: targetOrgName,
+        name: targetRepoName,
+        description: targetRepoDescription,
+        include_all_branches: false,
+        'private': true,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
     })
-    .catch(function (error) {
-        core.setOutput("repo-url", "");
-        core.setFailed(error.message);
-    });
+}
+
+main().then(function (response) {
+    console.log("Repo "+targetRepoName+' created successfully!');
+    core.setOutput("repo-url", "https://github.com/"+targetOrgName+"/"+targetRepoName);
+}).catch(function (error) {
+    core.setOutput("repo-url", "");
+    core.setFailed(error.message);
+});
